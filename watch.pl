db94d49e777ca0d4f224c5b9400dd860f77047c1
Mário <emailoftheyear@gmail.com>
2013-06-27 22:45:49 +0100

    Huge update!
    Did a bunch of changes, see diff.
    - Improved code organization
    - Transparent behaviour between typical dalnet watch system and PTnet's changes (raw 608)
    - Compact messages in PTnet watch list (one line instead of two)
    
    Todo:
    - Keep a per-network watch list

--- watch.pl
+++ watch.pl
@@ -1,216 +1,442 @@
-# Watch script para irssi
+#
+# dalnet ircd WATCH implementation for irssi
+# supports PTnet's NickServ extras
+#
+# Uses some client code from the original watch script by ThEbUtChE
+# http://scripts.irssi.org/scripts/watch.pl
+# Credits: thebutche@interec.org / http://www.nebulosa.org
+#
+# author:  neofreak@irc.ptnet.org
+#  email: igotbugs@ticklemynausea.net 
+#    web: http://www.ticklemynausea.net
+#
+#
+#                     ,,__
+#           ..  ..   / o._)                   .---.
+#          /--'/--\  \-'||        .----.    .'     '.
+#         /        \_/ / |      .'      '..'         '-.
+#       .'\  \__\  __.'.'     .'          Ã¬-._
+#         )\ |  )\ |      _.'
+#        // \\ // \\
+#       ||_  \\|_  \\
+#       '--' '--'' '--'
+#
+#
+# BASED ON: http://mirc.org/mishbox/reference/rawhelp6.htm
+# PTnet RFC (lol): http://ptnet.org/noticias_ptnet_novidades-na-rede
+#
+#
+# Usage:
+#
+# /watch          : shows online/offline nicknames
+# /watch update   : updates the server with your online nicknames
+# /watch status   : shows the status of your watch list
+# /watch add nick : adds the nick to your watch list
+# /watch del nick : removes the nick from your watch list
+# /watch help     : shows thsi help screen
+#
+# You will want to execute the "/watch update" command automatically
+# on connect.
+# 
+# 
 
-# watch script consiste en un pequeño script que interpreta
-# este novedoso sistema de notify que nos evita la tarea de
-# tener que comprobar cada X tiempo si alguien de nuestro notify
-# esta en el irc, este script solamente podra ser usado en redes
-# que lo permitan, como por ejemplo irc-hispano.
-
-$VERSION = '1.0';
+$VERSION = '3.0';
 %IRSSI = (
- authors     => 'ThEbUtChE',
- contact     => 'thebutche@interec.org',
- name        => 'Watch script',
- description => 'Uso del comando watch para irssi.',
- license     => 'BSD',
- url         => 'http://www.nebulosa.org',
- changed     => 'viernes, 17 de enero de 2003, 03:19:15 CET',
- bugs        => 'ninguno'
+ authors   => 'neofreak, ThEbUtChE',
+ contact   => 'thebutche@interec.org, igotbugs@ticklemynausea.net',
+ name    => 'Watch',
+ description => 'Watch script for IRSSI.',
+ license   => 'BSD',
+ url     => 'http://www.ticklemynausea.net',
+ changed   => 'June 2013',
+ bugs    => 'igotbugs@ticklemynausea.net'
 );
 
 use Irssi;
 use Irssi::Irc;
-use Irssi::TextUI;
 use POSIX qw(floor);
 use POSIX qw(strftime);
+use Data::Dumper;
 
-our @stack = qw();
+our %watchlist = {};
 
-sub watch_list
+my $msg_lb = "%K[%n";
+my $msg_rb = "%K]%n";
+
+#
+# misc. functions
+#
+################################################################################
+
+# Checks if we're in PTnet
+sub is_ptnet {
+  my $server = Irssi::active_server();
+  my $version = $server->{version};
+
+  # expected: dal4.6.Based.PTnet1.7.00
+  return $version =~ /PTnet/;
+}
+
+# Checks if we're cconnected
+sub is_connected {
+  my $server = Irssi::active_server();
+
+  return $server != null;
+}
+
+# Prints crap.
+sub irssi_output {
+  my $server = Irssi::active_server();
+
+  if (is_connected()) {
+    my $tag = $server->{tag};
+    Irssi::print "%K[%n$tag%K]%n @_", MSGLEVEL_CRAP
+  } else {
+    Irssi::print "@_", MSGLEVEL_CRAP
+  }
+}
+
+# Checks if a nick is already saved in the watchlist file
+sub is_in_list
 {
+  my ($ni) = @_;
+
+  my($file) = Irssi::get_irssi_dir."/watch";
+  my($nick);
+  local(*FILE);
+  open FILE, "< $file";
+  while (<FILE>) {
+    @nick = split;
+  if (lc(@nick[0]) eq lc($ni)) { return 1; }
+  }
+  close FILE;
+  return 0;
+}
+
+# Pads a nickname with spaces.
+sub LPad {
+  local($str, $len, $chr) = @_;
+  $chr = " " unless (defined($chr));
+  return substr(($chr x $len) . $str, -1 * $len, $len);
+}
+
+#
+# Client code: command handling, etc
+#
+################################################################################
+
+# /watch list
+# Lists all nicknames saved in the watch list
+sub cmd_watch_list
+{
+  if (is_connected()) {
+    Irssi::active_win()->command("quote watch l");
+  } else {
     my($file) = Irssi::get_irssi_dir."/watch";
     my($nick);
+    my $str = "";
+    my $c = 0;
     local(*FILE);
 
     open FILE, "< $file";
     while (<FILE>) {
-	@nick = split;
-	Irssi::print "Notify \002@nick[0]\002";
+      @nick = split;
+      $str .= " @nick[0]";
+      $c = $c + 1;
     }
+
     close FILE;
-}
 
-sub esta_notify
-{
-	my ($ni) = @_;
-
-    my($file) = Irssi::get_irssi_dir."/watch";
-    my($nick);
-    local(*FILE);
-    open FILE, "< $file";
-    while (<FILE>) {
-        @nick = split;
-	if (@nick[0] eq $ni) { return 1; }
+    if ($c > 0) {
+      irssi_output "Watching %_$c%n nicknames:$str";
+    } else {
+      irssi_output "Watch list is empty!";
     }
-    close FILE;
-return 0;
-}
-
-sub watch_add
-{
-	my ($nick) = @_;
-	my($file) = Irssi::get_irssi_dir."/watch";
-    local(*FILE);
-	if ($nick eq "") { Irssi::print "Please choose a nickname."; return; 
-	} elsif (esta_notify($nick)) { Irssi::print "The nickname is already on the watchlist."; return; }
-
-    open FILE, ">> $file";
-                print FILE join("\t","$nick\n");
-    close FILE;
-Irssi::print "The nick $nick has been added.";
-Irssi::active_win()->command("quote watch +$nick");
+  }
 
 }
 
-sub watch_del
+sub cmd_watch_status {
+  Irssi::active_win()->command("quote watch s");
+}
+
+# /watch add
+# Adds a nickname to the watch list
+sub cmd_watch_add
 {
-	my ($ni) = @_;
-        my($file) = Irssi::get_irssi_dir."/watch";
-        my($file2) = Irssi::get_irssi_dir."/.watch-temp";
-	    local(*FILE);
-	    local(*FILE2);
-        if ($ni eq "") { Irssi::print "Please choose a nickname."; return;
-        } elsif (!esta_notify($ni)) { Irssi::print "The nickname isn't on the watchlist."; return; }
+  my ($nick) = @_;
+  my($file) = Irssi::get_irssi_dir."/watch";
+  local(*FILE);
+  if ($nick eq "") { irssi_output "Please choose a nickname."; return; 
+  } elsif (is_in_list($nick)) { irssi_output "The nickname is already on the watchlist."; return; }
 
-    open FILE2, "> $file2";
-        print FILE2 "";
-    close FILE2;
+  open FILE, ">> $file";
+        print FILE join("\t","$nick\n");
+  close FILE;
 
-    open FILE, "< $file";
-    open FILE2, ">> $file2";
-    while (<FILE>) {
-        @nick = split;
-        if (@nick[0] eq $ni) { 
-	} else {
-                print FILE2 join("\t","@nick[0]\n");
-	}
+  if (is_connected) {
+    Irssi::active_win()->command("quote watch +$nick");
+  } else {
+    irssi_output "Nick %_$nick%n has been added to the.";
+  }
+}
+
+# /watch del
+# Removes a nickname from watch
+sub cmd_watch_del
+{
+  my ($ni) = @_;
+  my($file) = Irssi::get_irssi_dir."/watch";
+  my($file2) = Irssi::get_irssi_dir."/.watch-temp";
+  local(*FILE);
+  local(*FILE2);
+  
+  if ($ni eq "") { 
+    irssi_output "Please choose a nickname."; 
+    return;
+  } elsif (!is_in_list($ni)) { 
+    irssi_output "The nickname isn't on the watchlist.";
+    return;
+  }
+
+  open FILE2, "> $file2";
+    print FILE2 "";
+  close FILE2;
+
+  open FILE, "< $file";
+  open FILE2, ">> $file2";
+
+  while (<FILE>) {
+    @nick = split;
+    if (lc(@nick[0]) eq lc($ni)) { 
+    } else {
+      print FILE2 join("\t","@nick[0]\n");
     }
-    close FILE;
-    close FILE2;
+  }
 
-    open FILE, "> $file";
-	print FILE "";
-    close FILE;
+  close FILE;
+  close FILE2;
 
-    open FILE, ">> $file";
-    open FILE2, "< $file2";
-    while (<FILE2>) {
-        @nick = split;
-		print FILE join("\t","@nick[0]\n");
-    }
-    close FILE;
-    close FILE2;
+  open FILE, "> $file";
+  print FILE "";
+  close FILE;
 
-Irssi::active_win()->command("quote watch -$ni");
-Irssi::print "The nick \002$ni\002 has been deleted.";
+  open FILE, ">> $file";
+  open FILE2, "< $file2";
+
+  while (<FILE2>) {
+    @nick = split;
+    print FILE join("\t","@nick[0]\n");
+  }
+
+  close FILE;
+  close FILE2;
+
+  Irssi::active_win()->command("quote watch -$ni");
+
+  if (!is_connected()) {
+    irssi_output "Nick %9$ni%9 was removed from the watch list.";
+  }
 
 }
 
-sub watch_list_online
+#
+# /watch command
+# Main watch command
+sub cmd_watch 
 {
-Irssi::active_win()->command("quote watch l");
+  my ($arg) = @_;
+  my ($cmd, $nick) = split(/ /, $arg);
+
+  if ($cmd eq "help") {
+    cmd_watch_help();
+  } elsif ($cmd eq "add") {
+    cmd_watch_add($nick);
+  } elsif ($cmd eq "del") {
+    cmd_watch_del($nick);
+  } elsif ($cmd eq "update") {
+    cmd_watch_load();
+  } elsif ($cmd eq "debug") {
+    print Dumper(\%watchlist);
+  } elsif ($cmd eq "status") {
+    cmd_watch_status();
+  } else {
+    cmd_watch_list();
+  }
 }
 
-sub watch 
+sub cmd_watch_load
 {
-	my ($arg) = @_;
-	my ($cmd, $nick) = split(/ /, $arg);
-	if ($cmd eq "list") {
-		watch_list();
-	} elsif ($cmd eq "add") {
-		watch_add($nick);
-	} elsif ($cmd eq "del") {
-		watch_del($nick);
-	} elsif ($cmd eq "update") {
-		watch_load();
-	} else {
-		watch_list_online();
-	}
+  my($file) = Irssi::get_irssi_dir."/watch";
+  my($nick);
+  my $ret;
+
+  local(*FILE);
+  open FILE, "< $file";
+  while (<FILE>) {
+    @nick = split;
+    $ret .= "+@nick[0] ";
+  }
+  close FILE;
+  chop $ret;
+
+  Irssi::active_win()->command("quote watch $ret");
 }
 
-sub watch_load
+sub cmd_watch_help 
 {
-    my($file) = Irssi::get_irssi_dir."/watch";
-    my($nick);
-    local(*FILE);
-	my $ret;
-    open FILE, "< $file";
-    while (<FILE>) {
-        @nick = split;
-	$ret .= "+@nick[0] ";
-    }
-	chop $ret;
-	Irssi::print("Sending watch command: watch $ret");
-	Irssi::active_win()->command("quote watch $ret");
-    close FILE;
+ irssi_output " Usage:";
+ irssi_output "   /watch          : shows online/offline nicknames";
+ irssi_output "   /watch update   : updates the server with your online nicknames";
+ irssi_output "   /watch status   : shows the status of your watch list";
+ irssi_output "   /watch add nick : adds the nick to your watch list";
+ irssi_output "   /watch del nick : removes the nick from your watch list";
+ irssi_output "   /watch help     : shows this help screen";
 }
 
-sub event_is_online
+# Sends the WATCH l or WATCH s command
+sub server_watch
 {
-	my ($server, $data) = @_;
-	my ($me, $nick, $ident, $host) = split(/ /, $data);
-	
-	#push(@stack, "%g$nick%n");
-	Irssi::print "\002$nick\002 \0034[\003\002\002$ident\@$host\0034]\003 \0033is on IRC";
+  my ($w) = @_;
+  Irssi::active_win()->command("quote watch $w");
 }
 
-sub event_is_offline
+#
+# IRC Raw Events! 601 - 608
+#
+# 600
+sub event_rpl_logon
 {
-	my ($server, $data) = @_;
-	my ($me, $nick) = split(/ /, $data);
+  my ($server, $data) = @_;
+  my ($me, $nick, $ident, $host) = split(/ /, $data);
 
-
-	#push(@stack, "%r$nick%n");
-	Irssi::print "\002$nick\002 \0034has left IRC";
+  # if the network is PTnet then we want to save the output for the 608
+  # that comes.
+  if (is_ptnet()) {
+    $watchlist{$nick} = { 'ident' => $ident, 'host' => $host, 'origin' => '600' };
+  } else {
+    irssi_output "%G$nick%K [%n$ident\@$host%K]%G %9logged on";
+  }
 }
 
-sub null
+# 601
+sub event_rpl_logoff 
 {
+  my ($server, $data) = @_;
+  my ($me, $nick) = split(/ /, $data);
 
+  irssi_output "%R$nick%n logged off";
 }
 
-sub event_is_online_regnick {
-	my ($server, $data) = @_;
-	my ($me, $nick, $regtime) = split(/ /, $data);
+# 602
+sub event_rpl_watchoff {
+  my ($server, $data) = @_;
+  my ($me, $nick) = split(/ /, $data);
 
-	if ($regtime eq 301097) {
-		Irssi::print "$nick is online \0032and identified with a special value for the regtime.";
-	} elsif ($regtime > 0) {
-		$regtimef = strftime('%b %d %H:%M:%S %Y', localtime($regtime));
-		Irssi::print "$nick is online \0033and has registered to NickServ in $regtimef";
-	} else {
-		Irssi::print "$nick is online \0034but not identified to NickServ!";
-	}	
+  irssi_output "Nick %_$nick%n was removed from the watch list.";
 }
-Irssi::command_bind('watch', 'watch');
+
+# 603
+sub event_rpl_watchstat {
+  my ($server, $data) = @_;
+  my ($me, $nick) = split(/ /, $data);
+
+  my $msg = substr($data, index($data, ':') + 1);
+  irssi_output "%_$msg%n";
+}
+
+# 604 
+sub event_rpl_nowon {
+  my ($server, $data) = @_;
+  my ($me, $nick, $ident, $host) = split(/ /, $data);
+ 
+  # if the network is PTnet then we want to save the output for the 608
+  # that comes.
+  if (is_ptnet()) {
+    $watchlist{$nick} = { 'ident' => $ident, 'host' => $host, 'origin' => '604'};
+  } else {
+    irssi_output "%G$nick%K [%n$ident\@$host%K]%G %9is online!";
+  }
+ 
+}
+
+# 605
+sub event_rpl_nowoff {
+  my ($server, $data) = @_;
+  my ($me, $nick, $ident, $host) = split(/ /, $data);
+
+  irssi_output "%R$nick%n is offline!";
+}
+
+# 606
+sub event_rpl_watchlist {
+  my ($server, $data) = @_;
+  my ($me, $nick) = split(/ /, $data);
+
+  my $msg = substr($data, index($data, ':') + 1);
+  irssi_output "%_Watching:%n $msg";
+}
+
+# 607
+sub event_rpl_endofwatchlist {
+  my ($server, $data) = @_;
+  
+  my $msg = substr($data, index($data, ' ') + 1);
+  
+  if ($msg =~ /WATCH l/) {
+    irssi_output "End of %_watch list%n";
+  } elsif ($msg =~ /End of WATCH s/) {
+    irssi_output "End of %_watch status%n";
+  }
+}
+
+# 608
+# PTnet only
+sub event_rpl_watchnickserv {
+  my ($server, $data) = @_;
+  my ($me, $nick, $regtime) = split(/ /, $data);
+  my $mask, $event_str;
+
+  $mask = "$watchlist{$nick}{'ident'}\@$watchlist{$nick}{'host'}";
+  if ($watchlist{$nick}{'origin'} == 600) {
+    $event_str = "logged on!"
+  } else {
+    $event_str = "is online"
+  }
+  
+
+  #$nick = LPad($key, 15);
+  # lool PTnet is funny and returns a weird value for NickServ etc lelele
+  if ($regtime eq 301097) {
+    #irssi_output "%G>> $nick %K$msg_lb%n$mask%K$msg_rb%n is online%n %K/%n %BServices Nickname";
+    irssi_output "%G$nick%n $msg_lb"."Services Nickname ($mask) Services Nickname)$msg_rb $event_str";
+  } elsif ($regtime > 0) {
+    $regtimef = strftime('%b %d %H:%M:%S %Y', localtime($regtime));
+    irssi_output "%G$nick%n $msg_lb"."since $regtimef ($mask)$msg_rb $event_str";
+  } elsif ($regtime == 0) {
+    #irssi_output "%G>> $nick %K$msg_lb%n$mask%K$msg_rb%n is online%n %K/%n %Rnot registered or identified%n";
+    irssi_output "%R$nick%n $msg_lb"."not registered or not identified ($mask)$msg_rb $event_str";
+  } else {
+    irssi_output "%RError:%n unknown value for regtime in RAW_608";
+  }
+}
+
+Irssi::command_bind('watch', 'cmd_watch');
 #Irssi::signal_add_last('event connected', 'watch_load');
-Irssi::signal_add('event 604', 'event_is_online');
-Irssi::signal_add('event 605', 'null');
-Irssi::signal_add('event 601', 'event_is_offline');
-Irssi::signal_add('event 600', 'event_is_online');
-Irssi::signal_add('event 608', 'event_is_online_regnick');
+
+Irssi::signal_add('event 600', 'event_rpl_logon');
+Irssi::signal_add('event 601', 'event_rpl_logoff');
+Irssi::signal_add('event 602', 'event_rpl_watchoff');
+Irssi::signal_add('event 603', 'event_rpl_watchstat');;
+Irssi::signal_add('event 604', 'event_rpl_nowon');
+Irssi::signal_add('event 605', 'event_rpl_nowoff');
+Irssi::signal_add('event 606', 'event_rpl_watchlist');
+Irssi::signal_add('event 607', 'event_rpl_endofwatchlist');
+
+Irssi::signal_add('event 608', 'event_rpl_watchnickserv'); # PTnet!
 
 
-# Statusbar Item
-
-sub watch_queue {
-	my ($sb_item, $get_size_only) = @_;
-	my $sb;
-
-	$sb = "".@stack;
-	$sb = substr($sb, 30);
-# all draw functions should end with a call to default_handler().
-	$sb_item->default_handler($get_size_only, "{sb $sb}", '', 0);
-}
-
-#Irssi::statusbar_item_register ('watch_queue', 0, 'watch_queue');
+#
+# Contains ascii art by:
+#   http://www.chris.com/ascii/index.php?art=animals/camels
+#
